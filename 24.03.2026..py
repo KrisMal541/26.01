546b2d5da6997a1a25ebb3729961af8b8ebb4955
KrisMal541 <karinebrice451@gmail.com>
2026-03-24 09:50:35 +0200

    Add files via upload

---
+++ 24.03.2026..py
@@ -0,0 +1,17 @@
+import re
+with open ("klienti.txt","r",encoding="utf-8")as datne:
+    dati=datne.read()
+
+epasti=re.findall(r"\w+@\w+\.\w+",dati)
+print(epasti)
+print(len(epasti))
+telefoni=re.findall(r"\d{8}",dati)
+print(telefoni)
+
+
+aizvietots=re.sub(r"\d{8}","📱",dati)
+print(aizvietots)
+
+datne=open("klienti_anon.txt","w",encoding="utf-8")
+datne.write(aizvietots)
+datne.close()
